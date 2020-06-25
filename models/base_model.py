4443ef49860da17668d09642930dbf1855b956d0
AngieCastano1634 <60395533+AngieCastano1634@users.noreply.github.com>
2020-06-25 18:16:01 -0500

    works with unittest

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,13 @@
 #!/usr/bin/python3
-"""
-Class BaseModel that defines all common attributes/methods for other classes:
-"""
-
-from datetime import date
+'''
+Base Model
+'''
 import uuid
 
-class BaseModel():
-    def __init__(self):
-        """
-        Initializes class BaseModel
-        """
-        id = str(uuid.uuid4())
-        created_at = datetime.today()
-        updated_at = datetime.today()
 
-    def __str__(self):
-        """
-        The __str__ magic method that returns
-        the BaseModel description
-        """
-        return "[{}] ({}) {}".format(__class__, self.id, self.dict)
-        
-    def save(self):
-        """
-        Updates the public instance attribute update_at
-        with the current datetime
-        """
-        updated_at = datetime.today()
-    
-    def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
-        return self.__dict__
+class BaseModel:
+    ''' Base class manage id attribute in all your future classes '''
+
+    def __init__(self, id=None):
+        ''' Constructor method '''
+        self.id = uuid.uuid4()
