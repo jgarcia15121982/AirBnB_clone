e9d163a9ba281a0b1cf0917affe53cd9577af38e
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-26 02:49:10 +0000

    [UPDATE] Fixing some issues

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,42 @@
 '''
 Base Model
 '''
+import time
+from datetime import date
 import uuid
 
 
+
 class BaseModel:
-    ''' Base class manage id attribute in all your future classes '''
+    """ Base class manage id attribute in all your future classes """
 
     def __init__(self, id=None):
-        ''' Constructor method '''
-        self.id = uuid.uuid4()
+        """ Constructor method """
+        self.id = str(uuid.uuid4())
+        self.created_at = date.today()
+        self.updated_at = date.today()
+
+    def __str__(self):
+        """
+        The __str__ magic method that returns
+        the BaseModel description
+        """
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute update_at
+        with the current datetime
+        """
+        updated_at = date.today()
+
+    def to_dict(self):
+        """
+        Method that returns the dictionary
+        representation of the Base class
+        """
+        bs_dict = {}
+        bs_dict['id'] = self.id
+        bs_dict['created_at'] = self.created_at.isoformat()
+        bs_dict['updated_at'] = self.updated_at.isoformat()
+        return bs_dict
