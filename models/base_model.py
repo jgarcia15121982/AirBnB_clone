983232607b2b1a03dfc6ba619b136f655c1d898f
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-28 05:01:58 +0000

    [UPDATE] update to a method

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
-        storage.new(self.to_dict())
+        storage.new(self)
 
     def __str__(self):
         """
