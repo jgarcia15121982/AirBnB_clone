de231892431620670489e6288f3b45c8e15671ee
AngieCastano1634 <1634@holbertonschool.com>
2020-06-27 21:37:41 -0500

    [UPDATE][adding console.py file]

---
+++ console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""AirBnB console"""
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """Console of AirBnB"""
+    prompt = '(hbnb)'
+    def do_quit(self, args):
+        """Quit command, exits the program """
+        exit()
+    def do_EOF(self, args):
+        """ End of file, exits the program"""
+        exit()
+if __name__ = '__main__':
+    HBNBCommand().cmdloop()
