07bcb76d037c8eb23f6a9851f491d7e97d519228
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-28 05:05:06 +0000

    [FIX] located in a new directory

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""that serializes instances to a JSON file and deserializes JSON file to instances"""
+import json
+
+class FileStorage():
+    """serializes instances to a JSON file and deserializes JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        self.__objects[obj.__class__.__name__ + "." + obj.to_dict()["id"]] = obj.to_dict()
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        try:
+            with open(self.__file_path, mode="w+", encoding="utf-8") as File:
+                string = json.dumps(self.__objects)
+                File.write(string)
+        except(TypeError):
+            pass
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path) as json_file:
+                data = json.load(json_file)
+                self.__objects = data
+        except(FileNotFoundError):
+            pass
