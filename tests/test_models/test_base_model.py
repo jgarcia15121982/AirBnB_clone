2ff55cb3cbcc1da6ee308e23844f2ae9c6ec1fe3
jgarcia15121982 <johfregarcia@gmail.com>
2020-06-28 05:13:05 +0000

    [FIX] Shebang corrected

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,10 @@
-#!/usr/python3
+#!/usr/bin/python3
 '''
 tests
 '''
 import unittest
 from models.base_model import BaseModel
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 import datetime
 
 
