a91fa1436f37322f97f2dc4d79a21c7819dd59bc
AngieCastano1634 <1634@holbertonschool.com>
2020-06-30 13:46:39 -0500

    [fix][not using self.__objects but FileStorrage.__objects]

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,31 +10,31 @@ class FileStorage():
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         try:
-            with open(self.__file_path, mode="w+", encoding="utf-8") as File:
+            with open(FileStorage.__file_path, mode="w+",
+                      encoding="utf-8") as F:
                 copy = FileStorage.__objects.copy()
                 for key, value in copy.items():
                     copy[key] = value.to_dict()
                 string = json.dumps(copy)
-                File.write(string)
+                F.write(string)
         except(TypeError):
             pass
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path) as json_file:
+            with open(FileStorage.__file_path) as json_file:
                 dict_from_json = json.load(json_file)
                 for key, value in dict_from_json.items():
-                    self.__objects[key] = BaseModel(**value)
+                    FileStorage.__objects[key] = BaseModel(**value)
         except:
             pass
-
