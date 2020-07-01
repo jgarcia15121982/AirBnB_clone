e7c2041138f4f3ee1daf90fedbb40b0f9f76f878
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-01 15:35:38 -0500

    [UPDATE] adding *args y **kwargs

--- models/state.py
+++ models/state.py
@@ -9,6 +9,6 @@ class State(BaseModel):
     """class State that inherits from BaseModel"""
     name = ""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize State class"""
-        super().__init__()
+        super().__init__(*args, **kwargs)
