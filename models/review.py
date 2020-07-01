ccab1796b4fb3d24c0b27e3a444b3b3deef8bdee
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-01 15:36:29 -0500

    [UPDATE] adding *args y **kwargs

--- models/review.py
+++ models/review.py
@@ -11,6 +11,6 @@ class Review(BaseModel):
     user_id = ""
     text = ""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize User class"""
-        super().__init__()
+        super().__init__(*args, **kwargs)
