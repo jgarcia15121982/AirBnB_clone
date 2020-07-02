d95cd6f54d8cb040963bca9ab9c98ec098250084
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-01 20:52:01 -0500

    [UPDATE] PEP8 standard

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -6,6 +6,7 @@ import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
+
 class TestFileStorage(unittest.TestCase):
     """Unittest for class FileStorage"""
     def test_file_path(self):
